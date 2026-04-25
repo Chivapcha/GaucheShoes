449a4fd0bd8b24dbbe0bba362efdce29773f34de
Chivapcha <knysh.yana.3@gmail.com>
2026-04-25 14:41:45 +0200

    boutons ouvrir/fermer panier et compte marchent

---
+++ script.js
@@ -0,0 +1,13 @@
+function ouvrirPanier() {
+    document.getElementById("panier").style.visibility = "visible";
+}
+function fermerPanier() {
+    document.getElementById("panier").style.visibility = "hidden";
+}
+
+function ouvrirCompte() {
+    document.getElementById("compte").style.visibility = "visible";
+}
+function fermerCompte() {
+    document.getElementById("compte").style.visibility = "hidden";
+}
